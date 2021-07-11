6a1943075de32aded3d4bf6d10f402c2720c2010
jeziellecunha <jezielle.cunha@gmail.com>
2021-07-10 22:43:41 -0300

    primeiro comite, criação dos primeiros arquivos

---
+++ src/main/java/br/com/accentbank/accentBank/AccentBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.accentbank.accentBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/accentbank/accentBank/AccentBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.accentbank.accentBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
